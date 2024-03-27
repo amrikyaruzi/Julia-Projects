78820e485342c6a3eacf2d5317890dec1868ff04
Amri <amrikyaruzi@gmail.com>
2024-03-27 13:20:55 +0300

    .

--- Projects/1. Julia Packages after Fresh Installation.jl
+++ Projects/1. Julia Packages after Fresh Installation.jl
@@ -4,7 +4,7 @@ Pkg.add(
     ["Cascadia", "DataFrames", "DataFramesMeta", "GLM", "Gumbo", "HTTP", "JSON", "Tidier",
     "UrlDownload", "CSV", "IJulia", "Conda", "Distributions", "StatsBase", "Pipe", "Chain",
     "TidierVest", "Printf", "ToolipsCrawl", "PrettyTables", "XLSX", "ToolipsCrawl", "Format",
-    "RCall", "TickTock", "Glob]
+    "RCall", "TickTock", "Glob"]
     )
 
 pkg"add https://www.github.com/bicycle1885/Fmt.jl"
--- Projects/Closed Charts/Code/Closed Charts Analysis.jl
+++ Projects/Closed Charts/Code/Closed Charts Analysis.jl
@@ -9,8 +9,6 @@ using DataFramesMeta
 using Chain
 using RCall
 
-"""time1 = time()
-#tick()"""
 
 
 month_abb = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
@@ -18,7 +16,22 @@ what_months = month_abb[1:3] # start from Jan to current end month
 what_year = 2023
 
 # Reading files in
+## All other departments
+### Before July 2023
 path = "./Data/1. Raw data/2023/All/Before July 2023"
+
+
+"""
+function readcsvs(path)
+    files=glob("*.csv", path) #Vector of filenames. Glob allows you to use the asterisk.
+    numfiles=length(files)    #Number of files to read.
+    tempdfs=Vector{DataFrame}(undef, numfiles) #Create a vector of empty dataframes.
+    for i in 1:numfiles
+        tempdfs[i]=CSV.read(files[i]) #Read each CSV into its own dataframe.
+    end
+    masterdf=outerjoin(tempdfs..., on="Column In Common") #Join the temporary dataframes into one dataframe.
+end
+"""
 
 function read_csvs(path)
     files = glob("*.csv", path)
@@ -29,14 +42,64 @@ function read_csvs(path)
     end
 
     df = reduce(vcat, dfs)
+    #df = vcat(dfs...)
     return df
 end
 
-@time df = read_csvs(path)
 
-df[!, :Sample]
+df = read_csvs(path)
+# @time df = read_csvs(path)
+# t1 = time(); t2 = time(), t2-t1
+
+df[!, "Sample"]
+
+select!(df, Not(["Sample"]))
+
+"""
+@chain df begin
+    select(_, "Sample")
+    unique(_, "Sample")
+end
+"""
+
+println(names(df))
+length(names(df))
+
+for column in names(df)
+    println("COLUMN: $column is of TYPE: $(eltype(df[!, column]))")
+end
+
+### July 2023 onwards
+path2 = "./Data/1. Raw data/2023/All/July 2023 onwards/"
+files2 = glob("*.xlsx", path2)
+
+"""
+dfs2 = Vector{DataFrame}(undef, length(files2))
+
+for file in files2
+    tempdf = DataFrame(XLSX.readtable(file, "sheet1"))
+    #println(tempdf)
+    push!(dfs2, tempdf)
+end
+"""
+
+dfs2 = Vector{DataFrame}(undef, length(files2)) #Create a vector of empty dataframes.
+
+for i in eachindex(files2)
+    dfs2[i] = DataFrame(XLSX.readtable(files2[i], "sheet1"))
+end
+
+for i in eachindex(dfs2)
+    rename!(dfs2[i], ["S.No", "MR.No",	"Visit/Adm No",	"Patient Name",	"Visit/Adm Date", "Consultant Code",
+    "Consultant", "Department", "Ward Name", "Document Name", "Document Type",	"Doc SNN",
+    "Documented",	"Timely",	"Legible",	"Complete",	"Accurate"])
+end
+
+dfs2 = vcat(dfs2...)
+
+dfs2
 
 
-#tock()
-"""time2 = time()
-time2 - time1"""
+"""
+dfs_ = DataFrame.(XLSX.readtable.(files2, "sheet1"))
+"""
