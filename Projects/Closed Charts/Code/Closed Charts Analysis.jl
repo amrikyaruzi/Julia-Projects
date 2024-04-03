c066cf52545d299268586dcb846c8fff5605c001
Amri <amrikyaruzi@gmail.com>
2024-04-03 11:04:14 +0300

    .

--- Projects/Closed Charts/Code/Closed Charts Analysis.jl
+++ Projects/Closed Charts/Code/Closed Charts Analysis.jl
@@ -16,22 +16,12 @@ what_months = month_abb[1:3] # start from Jan to current end month
 what_year = 2023
 
 # Reading files in
-## All other departments
+
+## All departments except A&E
 ### Before July 2023
 path = "./Data/1. Raw data/2023/All/Before July 2023"
 
 
-"""
-function readcsvs(path)
-    files=glob("*.csv", path) #Vector of filenames. Glob allows you to use the asterisk.
-    numfiles=length(files)    #Number of files to read.
-    tempdfs=Vector{DataFrame}(undef, numfiles) #Create a vector of empty dataframes.
-    for i in 1:numfiles
-        tempdfs[i]=CSV.read(files[i]) #Read each CSV into its own dataframe.
-    end
-    masterdf=outerjoin(tempdfs..., on="Column In Common") #Join the temporary dataframes into one dataframe.
-end
-"""
 
 function read_csvs(path)
     files = glob("*.csv", path)
@@ -42,34 +32,28 @@ function read_csvs(path)
     end
 
     df = reduce(vcat, dfs)
-    #df = vcat(dfs...)
     return df
 end
 
 
 df = read_csvs(path)
-# @time df = read_csvs(path)
-# t1 = time(); t2 = time(), t2-t1
-
 df[!, "Sample"]
-
 select!(df, Not(["Sample"]))
 
-"""
-@chain df begin
-    select(_, "Sample")
-    unique(_, "Sample")
-end
-"""
 
 println(names(df))
 length(names(df))
 
 for column in names(df)
-    println("COLUMN: $column is of TYPE: $(eltype(df[!, column]))")
+    if column in names(df)
+        println("COLUMN: $column is of TYPE: $(eltype(df[!, column]))")
+    else
+        println("")
+    end
 end
 
 ### July 2023 onwards
+
 path2 = "./Data/1. Raw data/2023/All/July 2023 onwards/"
 
 function read_xlsxs_after_july2023(path)
@@ -79,12 +63,11 @@ function read_xlsxs_after_july2023(path)
 
     for i in eachindex(files2)
         dfs2[i] = DataFrame(XLSX.readtable(files2[i], "sheet1"))
-    end
-
-    for i in eachindex(dfs2)
-        rename!(dfs2[i], ["S.No", "MR.No",	"Visit/Adm No",	"Patient Name",	"Visit/Adm Date", "Consultant Code",
-        "Consultant", "Department", "Ward Name", "Document Name", "Document Type",	"Doc SNN",
-        "Documented",	"Timely",	"Legible",	"Complete",	"Accurate"])
+        
+        rename!(dfs2[i],
+        ["S.No", "MR.No", "Visit/Adm No", "Patient Name", "Visit/Adm Date", "Consultant Code",
+         "Consultant", "Department", "Ward Name", "Document Name", "Document Type",	"Doc SNN",
+         "Documented", "Timely", "Legible",	"Complete",	"Accurate"])
     end
 
     dfs2 = vcat(dfs2...)    
@@ -92,15 +75,49 @@ end
 
 @time df2 = read_xlsxs_after_july2023(path2)
 
-"""
-path2 = "./Data/1. Raw data/2023/All/July 2023 onwards/"
-files2 = glob("*.xlsx", path2)
 
-dfs2 = Vector{DataFrame}(undef, length(files2))
-
-for file in files2
-    tempdf = DataFrame(XLSX.readtable(file, "sheet1"))
-    #println(tempdf)
-    push!(dfs2, tempdf)
+for column in names(df2)
+    println("Column: $column is of type $(eltype(df2[!, column]))")
 end
+
+
+## A&E
+### Before July 2023
+
+df_ae_bjuly2023 = DataFrame(XLSX.readtable("./Data/1. Raw data/2023/All/Before July 2023/A&E/Microsoft Forms/A&E Closed Charts Audit(1-392).xlsx", "Sheet1"))
+select!(df_ae_bjuly2023, Not(Between("ID", "Name")))
+
+"""
+@chain df_ae_bjuly2023 begin
+    stack(_, Between("1.1 - Physician Initial Assessment All components - P",
+                      last(names(df_ae_bjuly2023))))
+    
+    select(_, Between("variable", last(names(_))))
+end #variable = DOCUMENTS, value = Values
+"""
+
+df_ae_bjuly2023 = stack(df_ae_bjuly2023, Between("1.1 - Physician Initial Assessment All components - P",
+                                         last(names(df_ae_bjuly2023))))
+
+transform!(df_ae_bjuly2023, :value => ByRow(x -> ismissing(x) ? [missing] : string.(split(x, ";"))) => :value)
+df_ae_bjuly2023 = flatten(df_ae_bjuly2023, "value")
+
+# filter(row -> !(ismissing(row.value) || row.value == ""), df_ae_bjuly2023)
+
+filter!(row -> !ismissing(row.value) && row.value != "", df_ae_bjuly2023)
+
+"""
+emergency_bj223 <- emergency_bj223 %>% separate_wider_delim(cols = Values, delim = "-", names = c("Values", "Responses"))
+
+emergency_bj223 <- emergency_bj223 %>% separate_wider_delim(cols = DOCUMENTS,
+                                                delim = " - ",
+                                                names = c("SNN", "DOCUMENTS", "TYPE"))
+
+
+emergency_bj223 <- emergency_bj223 %>%
+  group_by(`PATIENT NAMES`, `MR Number`, Month, Year, DOCTOR, RMO, NURSE, SNN, DOCUMENTS, TYPE,
+           Values) %>% filter(n() == 1) %>% ungroup()
+
+
+### July 2023 onwards
 """
